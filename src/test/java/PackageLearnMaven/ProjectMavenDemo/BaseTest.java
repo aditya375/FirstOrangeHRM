c7c7749aeb05ffc4bf30e58c0cb950c941381e92
aditya375 <adityaagnihotri.1991@gmail.com>
2020-01-17 14:58:46 +0530

    Master

--- src/test/java/PackageLearnMaven/ProjectMavenDemo/BaseTest.java
+++ src/test/java/PackageLearnMaven/ProjectMavenDemo/BaseTest.java
@@ -1,20 +1,28 @@
 package PackageLearnMaven.ProjectMavenDemo;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+	public WebDriver driver;
+
 	@Test
 	public void browserlaunch() {
-		String driverPath = System.getProperty("user.dir") + "\\src\\test\\java\\BrowserDrivers\\chromedriver.exe";
-		// System.out.println(javaVersion);
-		System.setProperty("webdriver.chrome.driver", driverPath);
-
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		// driver.get("https://opensource-demo.orangehrmlive.com/");
-	driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+		System.out.println("Browser launch");
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+
+//		String driverPath = System.getProperty("user.dir") + "\\src\\test\\java\\BrowserDrivers\\chromedriver.exe";
+//		// System.out.println(javaVersion);
+//		System.setProperty("webdriver.chrome.driver", driverPath);
+//
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		// driver.get("https://opensource-demo.orangehrmlive.com/");
+//	driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
 	}
 }
